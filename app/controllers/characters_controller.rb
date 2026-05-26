f20556daa162876183e42c0fb30795633d0b2e65
Theo Soubra Belay <TheoSoubraBelay@users.noreply.github.com>
2026-05-26 16:22:17 +0200

    edit + update

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,26 @@
+class CharactersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_character, only: [:edit, :update]
+
+  def edit
+    
+  end
+
+  def update
+    if @character.update(character_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_character
+    @character = current_user.characters.find(params[:id])
+  end
+
+  def character_params
+    params.require(:character).permit(:name, :personality, :race, :class, :gender, :history)
+  end
+end
