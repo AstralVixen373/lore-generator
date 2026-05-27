d54220faecf728e3482bf713c74e809acdce6cd0
guillaume dumaine <sydberny@gmail.com>
2026-05-27 16:34:44 +0200

    logo v2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,6 @@ class User < ApplicationRecord
   # for Google OmniAuth
   :omniauthable, omniauth_providers: [:google_oauth2]
 
-#   def self.from_omniauth(auth)
-#     # Find or create a user based on the provider and uid
-#     where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
-#       user.email = auth.info.email
-#       user.password = Devise.friendly_token[0, 20] # Generate a random password
-#     end
-#   end
-# end
-
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
       user.email      = auth.info.email
