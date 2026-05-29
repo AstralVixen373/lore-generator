16e18fc78a7daa78ac067d2e44061d45c48c2552
Lucie Aymeric <luciea3011@gmail.com>
2026-05-29 11:46:42 +0200

    Added dynamic interface to image generation

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,13 @@
 class Character < ApplicationRecord
   belongs_to :user
   has_many :chats, dependent: :destroy
+
+  after_update_commit -> {
+    broadcast_replace_to(
+      "character_#{id}",
+      target: "character_image",
+      partial: "characters/image",
+      locals: { character: self }
+    )
+  }
 end
