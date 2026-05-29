34ef2fe243108c3ddd3d1543a42d9684e8cfc22d
Theo Kapp <pro.theo.kapp@gmail.com>
2026-05-29 15:52:29 +0200

    final front 29.05

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,39 @@
 module ApplicationHelper
+  CHARACTER_TRANSLATIONS = {
+    race: {
+      "Human" => "Humain",
+      "Elf" => "Elfe",
+      "Dwarf" => "Nain",
+      "Orc" => "Orc",
+      "Half-elf" => "Demi-elfe",
+      "Draconians" => "Draconien"
+    },
+    role: {
+      "Thief" => "Voleur",
+      "Warrior" => "Guerrier",
+      "Spellcaster" => "Mage",
+      "Ranger" => "Rôdeur",
+      "Cleric" => "Clerc",
+      "Bard" => "Barde"
+    },
+    gender: {
+      "Male" => "Homme",
+      "Female" => "Femme",
+      "Non-binary" => "Non-binaire",
+      "Other" => "Autre"
+    },
+    personality: {
+      "Cold, cunning & independant" => "Froid, rusé et indépendant",
+      "Brave & loyal" => "Brave et loyal",
+      "Mysterious" => "Mystérieux",
+      "Chaotic" => "Chaotique",
+      "Wise" => "Sage"
+    }
+  }.freeze
+
+  def character_display_value(attribute, value, fallback: "Non renseigné")
+    return fallback if value.blank?
+
+    CHARACTER_TRANSLATIONS.fetch(attribute.to_sym, {}).fetch(value, value)
+  end
 end
